dfaa4f7dc97d001b1f49f301210a44e79e1d9e17
aapokat <aapo.katajala@gmail.com>
2023-11-22 11:27:42 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import Navbar from './Componenets/navbar/navbar';
 import Landing from './Componenets/landing/landing';
+import About from './Componenets/About/About';
+import Autopesut from './Componenets/Autopesut/Autopesut';
+import Tuotteet from './Componenets/Tuotteet_osio/Tuotteet';
+import Contacts from './Componenets/Contacts/Contacts';
+import Footer from './Componenets/Footer/Footer';
+
 
 
 function App() {
@@ -7,7 +13,11 @@ function App() {
     <div className="app">
       <Navbar/>
       <Landing/>
-    
+      <About/>
+      <Autopesut/>
+      <Tuotteet/>
+      <Contacts/>
+      <Footer/>
     </div>
   );
 }
