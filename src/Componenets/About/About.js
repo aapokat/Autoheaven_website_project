21917f32461053183dce7f6af988c8bebbb1c680
WaterProofComputer <52865588+WaterProofComputer@users.noreply.github.com>
2023-11-22 13:41:28 +0200

    about alotus

--- src/Componenets/About/About.js
+++ src/Componenets/About/About.js
@@ -5,7 +5,26 @@ import './About.css'
 const About = () => {
     return ( 
         <div className="about">
-           <h1>APUA</h1>
+            <div className="bigcontainer">
+               <div className="otsikkocontainer">
+               <h1 className="otsikko">Autopesu edullisesti moderneilla
+               ja tehokkailla autopesukoneilla</h1>
+               </div>
+               <div className="otsikkocontainer2">
+               <h1 className="otsikko">Miksi Valita AutoHeaven?</h1>
+               </div>
+               <div className="three-container">
+               <div className="box-33">
+               <p>Laatu</p>
+               </div>
+               <div className="box-33">
+               <p>Nopeus</p>
+               </div>
+               <div className="box-33">
+               <p>Toimipaikat</p>
+               </div>
+               </div>
+           </div>
         </div>
      );
 }
