085ac8f5dca069bf40cdf42d1227f70a91174a9c
aapokat <aapo.katajala@gmail.com>
2023-11-23 13:53:04 +0200

    Landing&Navbar update

--- src/Componenets/landing/landing.js
+++ src/Componenets/landing/landing.js
@@ -7,6 +7,9 @@ const landing = () => {
     return ( 
         <div className="landing">
            <video className="video" src={video} autoPlay muted></video> 
+           <div className="overlayText">
+            <h1>AutoHeaven - Missä autosi loistaa kirkkaammin kuin mikään taivaankappale. Palvelumme on enemmän kuin pesu, se on autollesi taivas maan päällä.</h1>
+           </div>
         </div>
      );
 }
--- src/Componenets/navbar/navbar.js
+++ src/Componenets/navbar/navbar.js
@@ -1,25 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './navbar.css';
 import Logo from '../assets/logo.png';
-import {Link} from 'react-scroll';
+import { Link } from 'react-scroll';
 
-const navbar = () => {
-    return ( 
-        <div className="navbar">
-            <div className="loko">
-                <img src={Logo} alt="Logo" className="Logo" />
-                <h1 className="lokoText">AutoHeaven</h1>
-            </div>
-            <div className="linkMenu">
-                <Link className="linkMenuItem">Etusivu</Link>
-                <Link className="linkMenuItem">Tieto</Link>
-                <Link className="linkMenuItem">Autopesut</Link>
-                <Link className="linkMenuItem">Tuotteet</Link>
-                <Link className="linkMenuItem">Ota yhteyttä</Link>
-            </div>
-        </div>
+const ScrolledDiv = ({ children, className }) => {
+  const [scrolled, setScrolled] = useState(false);
 
-     );
-}
- 
-export default navbar;
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 0;
+      if (isScrolled !== scrolled) {
+        setScrolled(isScrolled);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [scrolled]);
+
+  return (
+    <div className={`scrolled-div ${scrolled ? 'scrolled' : ''} ${className}`}>
+      {children}
+    </div>
+  );
+};
+
+const Navbar = () => {
+  return (
+    <ScrolledDiv className="navbar">
+      <div className="loko">
+        <img src={Logo} alt="Logo" className="Logo" />
+        <h1 className="lokoText">AutoHeaven</h1>
+      </div>
+      <div className="linkMenu">
+        <Link className="linkMenuItem">Etusivu</Link>
+        <Link className="linkMenuItem">Tieto</Link>
+        <Link className="linkMenuItem">Autopesut</Link>
+        <Link className="linkMenuItem">Tuotteet</Link>
+        <Link className="linkMenuItem">Ota yhteyttä</Link>
+      </div>
+    </ScrolledDiv>
+  );
+};
+
+export default Navbar;
