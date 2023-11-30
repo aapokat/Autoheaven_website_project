0c4f23a1902442a43740c21bfc335d943c9add1b
WaterProofComputer <waterproofcomputer@gmail.com>
2023-11-30 10:01:07 +0200

    tuote aloitus

--- src/Componenets/Tuotteet/1_Tuote.js
+++ src/Componenets/Tuotteet/1_Tuote.js
@@ -5,7 +5,14 @@ import './1_Tuote.css'
 const Tuote1 = () => {
     return ( 
         <div className="Tuote1">
+            <div className="tuotecontainer">
+                <div className="tuotekuva">
+                    <img src="/images/korrekpro.jpg"></img>
+                </div>
+
+
             
+            </div>
         </div>
      );
 }
