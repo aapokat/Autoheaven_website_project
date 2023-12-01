74412e9b080d1cf379fd0739fcdfcebc520538ce
WaterProofComputer <52865588+WaterProofComputer@users.noreply.github.com>
2023-12-01 12:38:01 +0200

    tuote1valmis

--- src/Componenets/Tuotteet/1_Tuote.js
+++ src/Componenets/Tuotteet/1_Tuote.js
@@ -7,10 +7,29 @@ const Tuote1 = () => {
         <div className="Tuote1">
             <div className="tuotecontainer">
                 <div className="tuotekuvasäilö">
-                    <img classname="kuvatuote123" src="/images/korrekpro.jpg"></img>
+                    <img className="kuvatuote123" src="/images/korrekpro.jpg"></img>
                 </div>
                 <div className="tuoteesittely">
-                    <p>ajsndoaiwdaowbds</p>
+                    <h1 className="tuote1otsikko">Korrek Pro TFC Polymatrix pinnoite 350 ml</h1>
+                    <p className="tuote1teksti">Korrek Pro TFC Polymatrix on keraaminen pinnoite todella helppoon ja nopeaan 
+                    omatoimiseen autonhoitoon. SiO2-pohjainen keraaminen pinnoite antaa autolle
+                    loistavan kiillon sekä suojan likaa ja UV-säteitä vastaan. Polymatrix-pinnoite 
+                    muodostaa erittäin kestävän ja vettähylkivän suojan maali-, kumi-, muovi-, 
+                    alumiini- ja kromipinnoille jopa kuudeksi kuukaudeksi. Pinnoite tekee auton 
+                    pesemisestä helppoa, koska lika irtoaa pinnoilta vaivattomasti.</p>
+                    <div className="hintasaatavuus">
+                        <p className="hintatext">23.99€</p>
+                        <p className="saatavuustext">Saatavuus:</p>
+                        <div className="saatavuuscontainer">
+                            <div className="saatavuustilanne">
+                            </div>
+                            <div className="saatavuustextcont">
+                                <p className="saatavuustext">
+                                Oulu
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
             
