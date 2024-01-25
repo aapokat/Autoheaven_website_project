f22cb406f5bcfe1e02aa76bdb7e49bc5bb25c90e
aapokat <aapo.katajala@gmail.com>
2024-01-25 09:59:32 +0200

    Update landing.js

--- src/Componenets/landing/landing.js
+++ src/Componenets/landing/landing.js
@@ -1,12 +1,14 @@
 import React from "react";
 import video from "../assets/Video1.mp4";
+import phooto from "../assets/toimipaikat_img.jpg"
 import './landing.css'
 
 
 const landing = () => {
     return ( 
         <div className="landing" id="landing-section">
-           <video className="video" src={video} autoPlay muted></video> 
+           {/* <video className="video" src={video} autoPlay muted></video>  */}
+           
            <div className="overlayText">
             <h1>AutoHeaven - Missä autosi loistaa kirkkaammin kuin mikään taivaankappale. Palvelumme on enemmän kuin pesu, se on autollesi taivas maan päällä.</h1>
            </div>
